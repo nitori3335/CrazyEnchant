11f543fe5f77b49a1ca2a92ec7127fc029c1e19f
nitori3335 <shizuku921800@gmail.com>
2026-01-06 08:47:14 +0900

    importの最適化

--- src/main/java/org/n_factory/crazyenchant/init/ModEnchantments.java
+++ src/main/java/org/n_factory/crazyenchant/init/ModEnchantments.java
@@ -9,11 +9,11 @@ import org.n_factory.crazyenchant.enchantment.*;
 
 public class ModEnchantments {
     public static final DeferredRegister<Enchantment> ENCHANTMENT = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS,Crazyenchant.MODID);
-    public static final RegistryObject<Enchantment> FISHHATER = ENCHANTMENT.register("fishhater", () -> new FishhaterEnchantment());
-    public static final RegistryObject<Enchantment> MULTIHITS = ENCHANTMENT.register("multihits", () -> new MultihitsEnchantment());
-    public static final RegistryObject<Enchantment> RANDOM_EFFECT = ENCHANTMENT.register("random_effect", () -> new RandomEffectEnchantment());
-    public static final RegistryObject<Enchantment> SILKFORTUNE = ENCHANTMENT.register("silkfortune", () -> new SilkfortuneEnchantment());
-    public static final RegistryObject<Enchantment> STEALEFFECT = ENCHANTMENT.register("stealeffect", () -> new StealeffectEnchantment());
-    public static final RegistryObject<Enchantment> SWIFTFLIGHT = ENCHANTMENT.register("swiftflight", () -> new SwiftflightEnchantment());
-    public static final RegistryObject<Enchantment> WITCHHUNTER = ENCHANTMENT.register("witchhunter", () -> new WitchhunterEnchantment());
+    public static final RegistryObject<Enchantment> FISHHATER = ENCHANTMENT.register("fishhater", FishhaterEnchantment::new);
+    public static final RegistryObject<Enchantment> MULTIHITS = ENCHANTMENT.register("multihits", MultihitsEnchantment::new);
+    public static final RegistryObject<Enchantment> RANDOM_EFFECT = ENCHANTMENT.register("random_effect", RandomEffectEnchantment::new);
+    public static final RegistryObject<Enchantment> SILKFORTUNE = ENCHANTMENT.register("silkfortune", SilkfortuneEnchantment::new);
+    public static final RegistryObject<Enchantment> STEALEFFECT = ENCHANTMENT.register("stealeffect", StealeffectEnchantment::new);
+    public static final RegistryObject<Enchantment> SWIFTFLIGHT = ENCHANTMENT.register("swiftflight", SwiftflightEnchantment::new);
+    public static final RegistryObject<Enchantment> WITCHHUNTER = ENCHANTMENT.register("witchhunter", WitchhunterEnchantment::new);
 }

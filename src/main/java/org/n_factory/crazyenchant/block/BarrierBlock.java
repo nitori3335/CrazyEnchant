5569b30292d7d1f0353f7a7c4467c40df9271913
nitori3335 <shizuku921800@gmail.com>
2026-01-06 22:19:47 +0900

    コードの最適化

--- src/main/java/org/n_factory/crazyenchant/block/BarrierBlock.java
+++ src/main/java/org/n_factory/crazyenchant/block/BarrierBlock.java
@@ -1,7 +1,6 @@
 package org.n_factory.crazyenchant.block;
 
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;

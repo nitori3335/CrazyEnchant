a401f6d372a2e14d16585f9f71e7e1b6dcf2f763
nitori3335 <shizuku921800@gmail.com>
2026-02-03 08:34:51 +0900

    Delete src/main/java/org/n_factory/crazyenchant/ritual directory

--- src/main/java/org/n_factory/crazyenchant/ritual/Ritualkey.java
+++
@@ -1,9 +0,0 @@
-package org.n_factory.crazyenchant.ritual;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.Level;
-
-public class Ritualkey {
-    public record RitualKey(ResourceKey<Level> dimension, BlockPos pos) { }
-}
